af5f58cb920d14ffa511871a42f8df1ab12d941b
Liviu2911 <pirvanliviu29@gmail.com>
2025-06-10 16:05:59 +0300

    update

--- client/src/supabase.ts
+++ client/src/supabase.ts
@@ -2,6 +2,11 @@ import { createClient } from "@supabase/supabase-js";
 
 const url = "https://izpsrhzyunpvyudqurzk.supabase.co";
 const key = import.meta.env.VITE_SUPABASE_KEY;
+if (!key) {
+  console.log("on client");
+  console.log(process.env.VITE_SUPABASE_KEY);
+}
+
 const supabase = createClient(url, key);
 
 export default supabase;
--- server/src/supabase.ts
+++ server/src/supabase.ts
@@ -2,9 +2,9 @@ import { createClient } from "@supabase/supabase-js";
 import dotenv from "dotenv";
 dotenv.config();
 
-const supabase = createClient(
-  process.env.VITE_SUPABASE_URL || "",
-  process.env.VITE_SUPABASE_KEY || ""
-);
+const url = process.env.VITE_SUPABASE_URL;
+const key = process.env.VITE_SUPABASE_KEY;
+
+const supabase = createClient(url || "", key || "");
 
 export default supabase;
